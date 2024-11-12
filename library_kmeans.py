e288c9fa87a911b4652e5400387fd36ecb4016e2
diogocarrer <diogocarrer14@gmail.com>
2024-11-12 08:13:57 -0300

    imprimindo o melhor silhouette score (library)

--- library_kmeans.py
+++ library_kmeans.py
@@ -1,21 +1,25 @@
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
-import numpy as np
 from sklearn.datasets import load_iris
-
 
 data = load_iris()
 X = data.data
 
+best_score = -1
+best_k = None
+
 for k in [3, 5]:
     print(f"Clusterização para k = {k}")
 
-
-    kmeans = KMeans(n_clusters=k, init='random', n_init=10, random_state=42) 
+    kmeans = KMeans(n_clusters=k, init='random', n_init=10, random_state=42)
     kmeans.fit(X)  # Ajustar o modelo aos dados
-
     labels = kmeans.labels_
-
     sil_score = silhouette_score(X, labels)
 
     print(f"Silhouette Score: {sil_score}")
+
+    if sil_score > best_score:
+        best_score = sil_score
+        best_k = k
+
+print(f"\nO melhor Silhouette Score é {best_score} para k={best_k}.")
